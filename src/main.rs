b7e8e2ce7dc16bced59422e57d245d9b22e4db26
jmagine <jasonma5501@gmail.com>
2021-11-20 04:44:55 -0800

    v0.2.0 renamed to svt, factored bpm into sv determination, option for ignoring end keypoint bpm

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,13 @@ use std::fs::File;
 use std::io::{self, BufRead, Write};
 use std::path::Path;
 
+#[derive(Clone)]
+pub struct MapObject {
+  time: i32,
+  class: i32,
+  data: String,
+}
+
 #[derive(Default, NwgUi)]
 pub struct SVT {
   //TODO make this pixel-perfect pretty
@@ -27,7 +34,7 @@ pub struct SVT {
 
   //toggles note changes
   #[nwg_control(text: "Hits", size: (95, 20), position: (5, 80), check_state: CheckBoxState::Checked)]
-  note_check: nwg::CheckBox,
+  hit_check: nwg::CheckBox,
 
   //toggles barline changes
   #[nwg_control(text: "Barlines", size: (95, 20), position: (5, 100), check_state: CheckBoxState::Checked)]
@@ -41,15 +48,19 @@ pub struct SVT {
   #[nwg_control(text: "0", size: (20, 20), position: (105, 80))]
   offset_text: nwg::TextInput,
 
-  #[nwg_control(text: "offset", size: (95, 20), position: (130, 82))]
+  #[nwg_control(text: "offset", size: (55, 20), position: (130, 82))]
   offset_label: nwg::Label,
 
   //buffer time
   #[nwg_control(text: "3", size: (20, 20), position: (105, 105))]
   buffer_text: nwg::TextInput,
 
-  #[nwg_control(text: "buffer", size: (95, 20), position: (130, 107))]
+  #[nwg_control(text: "buffer", size: (55, 20), position: (130, 107))]
   buffer_label: nwg::Label,
+
+  //toggles inh line changes
+  #[nwg_control(text: "Ignore end BPM", size: (105, 20), position: (185, 80), check_state: CheckBoxState::Unchecked)]
+  eq_bpm_check: nwg::CheckBox,
 
   //input map filename
   #[nwg_control(text: "", size: (175, 23), position: (6, 186))]
@@ -82,19 +93,24 @@ pub struct SVT {
   #[nwg_resource(title: "Open File", action: nwg::FileDialogAction::Open, filters: "osu(*.osu)")]
   file_dialog: nwg::FileDialog,
 
-  all_points: RefCell<Vec<String>>,
-  inh_points: RefCell<Vec<String>>,
+  all_objs: RefCell<Vec<MapObject>>,
+  new_objs: RefCell<Vec<MapObject>>,
+  //all_points: RefCell<Vec<String>>,
+  //inh_points: RefCell<Vec<String>>,
   //inh_times: RefCell<Vec<i32>>,
-  hit_times: RefCell<Vec<i32>>,
-  bar_times: RefCell<Vec<i32>>,
+  //hit_times: RefCell<Vec<i32>>,
+  //bar_times: RefCell<Vec<i32>>,
 
-  new_points: RefCell<Vec<String>>,
+  //new_points: RefCell<Vec<String>>,
 }
 
 impl SVT {
   fn apply_sv(&self) {
     self.load_file();
 
+    self.new_objs.borrow_mut().clear();
+
+    //take first 2 inherited lines and parse them
     let cmd = self.inherited_text.text();
     let mut lines = cmd.lines();
     let start_line = lines.next();
@@ -115,6 +131,7 @@ impl SVT {
       println!("formatting issue: {}", cmd);
       return;
     }
+
     let t_offset = self.offset_text.text().parse::<i32>();
     let t_buffer = self.buffer_text.text().parse::<i32>();
 
@@ -125,79 +142,117 @@ impl SVT {
     let end_time = end_tokens[0].parse::<i32>();
     let end_bl = end_tokens[1].parse::<f32>();
     let end_vol = end_tokens[5].parse::<i32>();
-    //  println!("[apply] need exactly 2 timing points for start/end");
-    //  return;
 
     match (t_offset, t_buffer, start_time, start_bl, start_vol, end_time, end_bl, end_vol) {
       (Ok(t_off), Ok(t_buf), Ok(s_t), Ok(s_b), Ok(s_v), Ok(e_t), Ok(e_b), Ok(e_v)) => {
-        let s_s = -100.0 / s_b;
-        let e_s = -100.0 / e_b;
-        println!("[apply] t:{}->{} sv:{}->{} vol:{}->{}", s_t, e_t, s_s, e_s, s_v, e_v);
+        //all tokens valid
 
-        //validation
-        if s_t > e_t {println!("[apply] start time should be less than end time"); return;}
-        if s_s <= 0.0 || e_s <= 0.0 {println!("[apply] sv values should be positive (neg beatlength values"); return;} 
-        if s_v < 0 || s_v > 100 || e_v < 0 || e_v > 100 {println!("[apply] volumes should be within [0, 100]"); return;}
-        
-        //compute change per time tick
-        let t_diff = e_t - s_t;
-        let s_diff = e_s - s_s;
-        let v_diff = e_v - s_v;
-        let s_per_ms = s_diff / t_diff as f32;
-        let v_per_ms = v_diff as f32 / t_diff as f32;
-
-        self.new_points.borrow_mut().clear();
-
-        //TODO insert a match on interp mode to determine whether to SV notes or lines
-
-        //to apply changes to inherited points, iterate over all inh_points
-        for l in self.inh_points.borrow().iter() {
-          //no error validation needed, done when reading file
-          let inh_tokens: Vec<&str> = l.split(",").collect();
-          
-          let time = inh_tokens[0].parse::<i32>();
-          match time {
-            Ok(t) => {
-              //time inside range
-              if t >= s_t - t_buf && t <= e_t + t_buf {
-                let new_t = t + t_off;
-                let new_s = s_s + (t - s_t) as f32 * s_per_ms;
-                let new_b = -100.0 / new_s;
-                let new_v = (s_v as f32 + (t - s_t) as f32 * v_per_ms) as u32;
-                let new_point = format!("{},{},{},{},{},{},{},{}", new_t, new_b, inh_tokens[2], inh_tokens[3], inh_tokens[4], new_v, inh_tokens[6], inh_tokens[7]);
-                println!("[new] {}", new_point);
-                self.new_points.borrow_mut().push(new_point);
-              }
-            },
-            _ => {
-              println!("[apply] invalid time {}", l);
+        //determine bpm at starting point
+        let mut start_bpm = 160.0;
+        let mut end_bpm = 160.0;
+        for obj in self.all_objs.borrow().iter() {
+          if obj.class == 0 {
+            let obj_tokens: Vec<&str> = obj.data.split(",").collect();
+            if obj.time < s_t {
+              start_bpm = 60000.0 / obj_tokens[1].parse::<f32>().unwrap();
+            }
+            if obj.time < e_t {
+              end_bpm = obj_tokens[1].parse::<f32>().unwrap();
             }
           }
         }
 
-        let mut all_times: Vec<i32> = Vec::new();
+        //convert beatlength values to sv values
+        //sv = -100.0 / beatlength
+        //raw_sv = sv * bpm
+        let s_sv_raw = -100.0 / s_b * start_bpm;
+        let e_sv_raw = if self.eq_bpm_check.check_state() == nwg::CheckBoxState::Checked {
+          -100.0 / e_b * start_bpm
+        } else {
+          -100.0 / e_b * end_bpm
+        };
+        //let s_sv_raw = -100.0 / s_b * start_bpm;
+        //let e_sv_raw = -100.0 / e_b * end_bpm;
 
-        if self.barline_check.check_state() == nwg::CheckBoxState::Checked {
-          all_times.extend(self.bar_times.borrow().iter().cloned());
-        }
+        //TODO equalize sv based on bpm, which may change depending on uninherited line positions
+        //let s_seq = s_s * bpm;
+        //let e_seq = e_s * bpm;
 
-        if self.note_check.check_state() == nwg::CheckBoxState::Checked {
-          all_times.extend(self.hit_times.borrow().iter().cloned());
-        }
-        all_times.sort();
+        //debug print
+        println!("[apply] t:{}->{} raw sv:{}->{} vol:{}->{}", s_t, e_t, s_sv_raw, e_sv_raw, s_v, e_v);
 
-        //apply to applicable times
-        for t in all_times.iter() {
-          if t >= &(s_t - t_buf) && t <= &(e_t + t_buf) {
-            let new_t = t + t_off;
-            let new_s = s_s + (t - s_t) as f32 * s_per_ms;
-            let new_b = -100.0 / new_s;
-            let new_v = (s_v as f32 + (t - s_t) as f32 * v_per_ms) as u32;
+        //validation on input values
+        if s_t > e_t {println!("[apply] start time should be less than end time"); return;}
+        if s_sv_raw <= 0.0 || e_sv_raw <= 0.0 {println!("[apply] sv values should be positive (neg beatlength values"); return;} 
+        if s_v < 0 || s_v > 100 || e_v < 0 || e_v > 100 {println!("[apply] volumes should be within [0, 100]"); return;}
+        
+        //compute change per time tick
+        let t_diff = e_t - s_t;
+        let sv_diff = e_sv_raw - s_sv_raw;
+        let v_diff = e_v - s_v;
+        let sv_per_ms = sv_diff / t_diff as f32;
+        let v_per_ms = v_diff as f32 / t_diff as f32;
 
-            let new_point = format!("{},{},{},{},{},{},{},{}", new_t, new_b, start_tokens[2], start_tokens[3], start_tokens[4], new_v, start_tokens[6], start_tokens[7]);
-            println!("[new] {}", new_point);
+        let mut bpm = 160.0;
+        let mut meter = 4;
+        let mut sample_set = 0;
+        let mut sample_index = 0;
+        let mut effects = 0;
 
-            self.new_points.borrow_mut().push(new_point);
+        for obj in self.all_objs.borrow().iter() {
+          //println!("[apply] {} {} {}", obj.time, obj.class, obj.data);
+          let obj_tokens: Vec<&str> = obj.data.split(",").collect();
+          //handle uninherited lines differently
+          if obj.class == 0 {
+            bpm = 60000.0 / obj_tokens[1].parse::<f32>().unwrap();
+            meter = obj_tokens[2].parse::<i32>().unwrap();
+            sample_set = obj_tokens[3].parse::<i32>().unwrap();
+            sample_index = obj_tokens[4].parse::<i32>().unwrap();
+            effects = obj_tokens[7].parse::<i32>().unwrap();
+            continue;
+          }
+
+          //perform general calculations here for inher, barlines, hitobjects
+          let obj_time = obj.time;
+          if obj_time >= s_t - t_buf && obj_time <= e_t + t_buf {
+            let new_t = obj_time + t_off;
+            let new_sv = s_sv_raw + (obj_time - s_t) as f32 * sv_per_ms;
+            let new_b = -100.0 / (new_sv / bpm);
+            let new_v = (s_v as f32 + (obj_time - s_t) as f32 * v_per_ms) as u32;
+            let new_point = format!("{},{},{},{},{},{},{},{}", new_t, new_b, meter, sample_set, sample_index, new_v, 0, effects);
+          
+
+            match obj.class {
+              0 => {println!("[apply] shouldn't get here, class 0");}, //uninherited line
+              1 => {
+                //inherited line
+                if self.inh_check.check_state() == nwg::CheckBoxState::Checked {
+                  println!("[new] inh {}", new_point);
+                  self.new_objs.borrow_mut().push(MapObject{time: new_t, class: 4, data: new_point});
+
+                  sample_set = obj_tokens[3].parse::<i32>().unwrap();
+                  sample_index = obj_tokens[4].parse::<i32>().unwrap();
+                  effects = obj_tokens[7].parse::<i32>().unwrap();
+                }
+              },
+              2 => {
+                //barline
+                if self.barline_check.check_state() == nwg::CheckBoxState::Checked {
+                  println!("[new] bar {}", new_point);
+                  self.new_objs.borrow_mut().push(MapObject{time: new_t, class: 4, data: new_point});
+                }
+              },
+              3 => {
+                //hitobject
+                if self.hit_check.check_state() == nwg::CheckBoxState::Checked {
+                  println!("[new] hit {}", new_point);
+                  self.new_objs.borrow_mut().push(MapObject{time: new_t, class: 4, data: new_point});
+                }
+              },
+              _ => {
+                println!("[apply] unknown class {}", obj.class);
+              }
+            }
           }
         }
       },
@@ -208,12 +263,18 @@ impl SVT {
     }
 
     //merge new points into old ones - delete old point if new one is identical
-    let mut output_points: Vec<String> = Vec::new();
-    output_points.extend(self.new_points.borrow().iter().cloned());
-    output_points.extend(self.all_points.borrow().iter().cloned());
-    output_points.sort_by_key(|k| (k.split(",").nth(0).unwrap().parse::<i32>().unwrap(), k.split(",").nth(6).unwrap().parse::<i32>().unwrap()));
-    output_points.dedup_by_key(|k| (k.split(",").nth(0).unwrap().parse::<i32>().unwrap(), k.split(",").nth(6).unwrap().parse::<i32>().unwrap()));
-    //println!("{:?}", output_points);
+    let mut out_objs: Vec<MapObject> = Vec::new();
+    out_objs.extend(self.new_objs.borrow().iter().cloned());
+    for obj in self.all_objs.borrow().iter() {
+      //uninherited/inherited lines
+      if obj.class == 0 || obj.class == 1 {
+        out_objs.push(obj.clone());
+      }
+    }
+    //out_objs.extend(self.all_objs.borrow().iter().cloned());
+    out_objs.sort_by_key(|k| (k.time, k.data.split(",").nth(6).unwrap().parse::<i32>().unwrap()));
+    out_objs.dedup_by_key(|k| (k.time, k.data.split(",").nth(6).unwrap().parse::<i32>().unwrap()));
+
     //all output is same except for timing points section
 
     //write out new file
@@ -237,8 +298,8 @@ impl SVT {
               bool_timing = true;
               out_string += &s;
               out_string += "\n";
-              for o_point in output_points.iter() {
-                out_string += &o_point;
+              for out_obj in out_objs.iter() {
+                out_string += &out_obj.data;
                 out_string += "\n";
               }
               out_string += "\n";
@@ -291,8 +352,19 @@ impl SVT {
   pub fn fill_out_filename(&self) {
     if self.preview_check.check_state() == nwg::CheckBoxState::Checked {
       let in_filename = &self.in_filename.text();
-      let folder = Path::new(in_filename).parent().unwrap();
-      self.out_filename.set_text(&format!("{}/{}", folder.to_str().unwrap(), "preview.osu"));
+
+      //prevent paths without parents or filenames from crashing
+      let folder = Path::new(in_filename).parent();
+      let name_osu = Path::new(in_filename).file_name();
+
+      match (folder, name_osu) {
+        (Some(folder), Some(name_osu)) => {
+          self.out_filename.set_text(&format!("{}/{}[{}].osu", folder.to_str().unwrap(), name_osu.to_str().unwrap().split("[").nth(0).unwrap(), "preview"));
+        },
+        _ => {
+          println!("[pre] path invalid");
+        },
+      }
     } else {
       self.out_filename.set_text(&self.in_filename.text());
     }
@@ -330,11 +402,10 @@ impl SVT {
     //self.inh_times = Vec::new();
     //self.hit_times = Vec::new();
 
-    self.all_points.borrow_mut().clear();
-    self.inh_points.borrow_mut().clear();
-    self.hit_times.borrow_mut().clear();
-    self.bar_times.borrow_mut().clear();
-
+    self.all_objs.borrow_mut().clear();
+    //self.inh_points.borrow_mut().clear();
+    //self.hit_times.borrow_mut().clear();
+    //self.bar_times.borrow_mut().clear();
 
     // read file line by line
     if let Ok(lines) = read_lines(&filename) {
@@ -367,16 +438,16 @@ impl SVT {
                   //self.inh_times.borrow_mut().push(time);
                 //}
 
-                let time_tokens: Vec<&str> = s.split(",").collect();
-                if time_tokens.len() != 8 {
+                let s_tokens: Vec<&str> = s.split(",").collect();
+                if s_tokens.len() != 8 {
                   continue;
                 }
 
-                let time = time_tokens[0].parse::<i32>();
-                let beatlength = time_tokens[1].parse::<f32>();
-                let meter = time_tokens[2].parse::<i32>();
-                let uninherited = time_tokens[6].parse::<i32>();
-                let effect = time_tokens[7].parse::<i32>();
+                let time = s_tokens[0].parse::<i32>();
+                let beatlength = s_tokens[1].parse::<f32>();
+                let meter = s_tokens[2].parse::<i32>();
+                let uninherited = s_tokens[6].parse::<i32>();
+                let effect = s_tokens[7].parse::<i32>();
                 
                 match (time, beatlength, meter, uninherited, effect) {
                   (Ok(t), Ok(bl), Ok(m), Ok(uninh), Ok(eff)) => {
@@ -384,23 +455,25 @@ impl SVT {
 
                     while bar_time + bar_inc < t as f32 {
                       bar_time += bar_inc;
-                      self.bar_times.borrow_mut().push(bar_time as i32);
+                      self.all_objs.borrow_mut().push(MapObject{time: bar_time as i32, class: 2, data: String::from("")});
                     }
 
                     if uninh == 1 {
                       //uninherited point
+                      self.all_objs.borrow_mut().push(MapObject{time: t, class: 0, data: s.clone()});
 
                       //only skip barline if effect bit 3 is set
                       if eff & 8 != 8 {
-                        self.bar_times.borrow_mut().push(t);
+                        self.all_objs.borrow_mut().push(MapObject{time: t, class: 2, data: String::from("")});
                       }
+
+                      //set barline counter based on uninherited point
                       bar_time = t as f32;
                       bar_inc = bl * m as f32;
                     } else if uninh == 0 {
                       //inherited point
-                      self.inh_points.borrow_mut().push(s.clone());
+                      self.all_objs.borrow_mut().push(MapObject{time: t, class: 1, data: s.clone()});
                     }
-                    self.all_points.borrow_mut().push(s.clone());
                   },
                   _ => {
                     println!("[load] issue {}", s);
@@ -409,7 +482,7 @@ impl SVT {
                 }
               } else if bool_hit {
                 if let Ok(hit_time) = s.split(",").nth(2).unwrap().parse::<i32>() {
-                  self.hit_times.borrow_mut().push(hit_time);
+                  self.all_objs.borrow_mut().push(MapObject{time: hit_time, class: 3, data: String::from("")});
                 }
               } else {
                 continue;
@@ -419,6 +492,7 @@ impl SVT {
         }
       }
     }
+    self.all_objs.borrow_mut().sort_by_key(|k| k.time);
     self.status.set_text(0, &format!("editing {}", Path::new(&filename).file_name().unwrap().to_str().unwrap()));
   }
 }
